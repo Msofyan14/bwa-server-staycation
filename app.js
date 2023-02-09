6df8d54853a005f86ec8b2763d5c5c3353b5525d
“Msofyan14” <mxsofyan14@gmail.com>
2023-02-10 02:44:29 +0700

    change

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/sb-admin-2",
-  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin"))
 );
 
 app.use("/", indexRouter);
