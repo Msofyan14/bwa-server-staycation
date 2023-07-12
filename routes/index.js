b9c12102073b5220099b212d582192bfee8277b3
“Msofyan14” <mxsofyan14@gmail.com>
2023-07-13 02:55:55 +0700

    semoga bisa

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.redirect("/admin/signin");
   req.session.touch();
+  res.redirect("/admin/signin");
 });
 
 module.exports = router;
